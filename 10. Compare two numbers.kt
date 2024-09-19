c918e4b2d8b1bfec142b9a20756a53b0d1d07dd6
Gustavo Rocha <97960688+gu-rocha@users.noreply.github.com>
2024-09-19 12:52:41 -0300

    Update 10. Compare two numbers.kt

--- 10. Compare two numbers.kt
+++ 10. Compare two numbers.kt
@@ -1,1 +1,18 @@
+fun main() {
+    // Test cases
+    val result1 = hasSpentMoreTimeToday(300, 250) // Should return true
+    val result2 = hasSpentMoreTimeToday(300, 300) // Should return false
+    val result3 = hasSpentMoreTimeToday(200, 220) // Should return false
+    val result4 = hasSpentMoreTimeToday(221, 220) // Should return true
+   
+    // Print the results
+    println("Result 1: $result1") 
+    println("Result 2: $result2") 
+    println("Result 3: $result3") 
+    println("Result 4: $result4")     
+}
 
+fun hasSpentMoreTimeToday(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
+    // Compare the time spent today with the time spent yesterday
+    return timeSpentToday > timeSpentYesterday
+}
