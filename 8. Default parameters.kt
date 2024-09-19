ca5cee7f57ef92309b72ce0063aa21d4cab648ed
Gustavo Rocha <97960688+gu-rocha@users.noreply.github.com>
2024-09-18 21:29:30 -0300

    Update 8. Default parameters.kt

--- 8. Default parameters.kt
+++ 8. Default parameters.kt
@@ -1,3 +1,5 @@
+// Step 1
+
 fun main() {
     val operatingSystem = "Chrome OS"
     val emailId = "sample@gmail.com"
@@ -8,5 +10,43 @@ fun main() {
 // Define your displayAlertMessage() below this line.
 
 fun displayAlertMessage (operatingSystem: String, emailId: String): String {
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+}
+
+// Step 2
+
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+  
+    // The following line of code assumes that you named your parameter as emailId. 
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+    
+    val fourthUserOperatingSystem = "Ubuntu"
+    val fourthUserEmailId = "user_four@gmail.com"
+
+    println(displayAlertMessage(fourthUserOperatingSystem, fourthUserEmailId))
+    println()
+}
+
+fun displayAlertMessage(emailId: String): String {
+    val noOperatingSystem = "Unknow OS"
+    return "There's a new sign-in request on $noOperatingSystem for your Google Account $emailId."
+}
+
+fun displayAlertMessage(operatingSystem: String, emailId: String): String {
     return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
 }
