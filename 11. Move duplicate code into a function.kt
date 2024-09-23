3cefafda76235b9ccd5e5b8b3a77652ce35745a4
Gustavo Rocha <97960688+gu-rocha@users.noreply.github.com>
2024-09-23 12:59:35 -0300

    Update 11. Move duplicate code into a function.kt

--- 11. Move duplicate code into a function.kt
+++ 11. Move duplicate code into a function.kt
@@ -1,3 +1,4 @@
+/*
 fun main() {
     println("City: Ankara")
     println("Low temperature: 27, High temperature: 31")
@@ -19,3 +20,22 @@ fun main() {
     println("Chance of rain: 7%")
     println()
 }
+*/
+
+fun main() {
+    val cities = listOf(
+        City("Ankara", 27, 31, 82),
+        City("Tokyo", 32, 36, 10),
+        City("Cape Town", 59, 64, 2),
+        City("Guatemala City", 50, 55, 7)
+    )
+
+    for (city in cities) {
+        println("City: ${city.name}")
+        println("Low temperature: ${city.lowTemp}, High temperature: ${city.highTemp}")
+        println("Chance of rain: ${city.chanceOfRain}%")
+        println()
+    }
+}
+
+data class City(val name: String, val lowTemp: Int, val highTemp: Int, val chanceOfRain: Int)
